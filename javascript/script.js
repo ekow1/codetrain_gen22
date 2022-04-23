8ed85db39333c006b4d1f3d0211a76b1de30e7f6
ekow enu <ekowfirmino@gmail.com>
2022-04-23 15:19:30 +0000

    javascript prac

---
+++ javascript/script.js
@@ -0,0 +1,20 @@
+let height , width , length , table , chair ;
+
+height = 20;
+
+width = 15;
+
+length = 25;
+
+
+table = width + height ;
+
+chair = length * height;
+
+console.log( height)
+console.log( length)
+console.log( width )
+console.log( table )
+console.log( chair )
+
+
